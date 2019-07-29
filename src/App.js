5ff23029e1d5416483f5fe4de23317285b54ce3c
ouweiya <Hi-Outlok@outlook.com>
2019-07-29 10:22:19 +0800

    ''

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React, { Fragment as f, createElement as e, useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
-const List = () => {
-  const [list, setList] = useState([]);
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(2),
+    marginBottom: 8,
+    transition: '0.35s'
+  },
+  link: {
+    '&:hover > div': {
+      background: '#eee',
+      boxShadow: '0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12)'
+    }
+  }
+}));
+
+export default function PaperSheet() {
+  const c = useStyles();
+  const [Gtihub, getGithub] = useState([]);
+
   useEffect(() => {
-    (async callback => {
-      try {
-        const url = `https://api.github.com/users/ouweiya/repos?sort=updated`;
-        const response = await fetch(url);
-        const responseBody = await response.json();
-        const names = await responseBody.map(git => git.name);
-        const lis = names.map((name, i) => e('li', { key: i }, name));
-        setList(lis);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
+    fetch(`https://api.github.com/users/ouweiya/repos?sort=updated`)
+      .then(res => res.json())
+      .then(res => getGithub(res));
   }, []);
 
-  return list;
-};
-
-export default e(f, null, e(List));
+  return Gtihub.map(git =>
+    e(
+      Link,
+      { key: git.id, href: git.html_url, target: '_blank', className: c.link, underline: 'none' },
+      e(
+        Paper,
+        { className: c.root },
+        e(Typography, { variant: 'h5', component: 'h3', gutterBottom: true }, git.name),
+        e(Typography, { variant: 'subtitle1', component: 'p', className: c.p }, git.description)
+      )
+    )
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(
+  <Container maxWidth='md' style={{ padding: '8px 0' }}>
+    <CssBaseline />
+    <App />
+  </Container>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
