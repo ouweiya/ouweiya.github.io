e70cd4968216cb76aa3f71b8b582cdf89babfbce
ouweiya <Hi-Outlok@outlook.com>
2019-09-03 14:13:17 +0800

    ''

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/stable";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
 
 ReactDOM.render(
-  <Container maxWidth='md' style={{ padding: '8px 0' }}>
+  <Container maxWidth="md" style={{ padding: "8px 0" }}>
     <CssBaseline />
     <App />
   </Container>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
+
+
+
+
