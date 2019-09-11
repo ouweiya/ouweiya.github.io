964967d2cb604a8bfe3ac711bd73e87f1340a33f
ouweiya <Hi-Outlok@outlook.com>
2019-09-11 22:29:58 +0800

    Updates

---
+++ precache-manifest.72fa458c4e457dc8c41f5e5e151fe184.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82cae49ccfe17d07febdbbcbfea97bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c83cb7121d3c190e7e8",
+    "url": "/static/js/2.30316e74.chunk.js"
+  },
+  {
+    "revision": "2410f1a9403c85f17c6f",
+    "url": "/static/js/main.4bb1f81e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
